342ec988c74a9cee7b86b0c4caeb368feb2d400c
Khang Dinh <21123341.khang@student.iuh.edu.vn>
2025-09-23 17:26:28 +0700

    articles page

---
+++ create_pages.ps1
@@ -0,0 +1,30 @@
+﻿# Script tạo các trang tự động
+ = @{
+    'vo-sinh-nam' = 'Vô sinh nam'
+    'xuat-tinh-som' = 'Xuất tinh sớm'
+    'viem-bang-quang' = 'Viêm bàng quang'
+    'benh-tuyen-tien-liet' = 'Bệnh tuyến tiền liệt'
+    'tieu-nhieu' = 'Tiểu nhiều'
+    'viem-co-tu-cung' = 'Viêm cổ tử cung'
+    'viem-buong-trung' = 'Viêm buồng trứng'
+    'u-nang-buong-trung' = 'U nang buồng trứng'
+    'dau-bung-kinh' = 'Đau bụng kinh'
+    'benh-lau' = 'Bệnh lậu'
+    'benh-giang-mai' = 'Bệnh giang mai'
+}
+
+foreach ( in .GetEnumerator()) {
+     = .Key
+     = .Value
+    
+    # Copy template từ liet-duong.html
+     = Get-Content "html\liet-duong.html" -Raw
+    
+    # Thay thế title và các thông tin cụ thể
+     =  -replace 'Liệt dương', 
+     =  -replace 'liet-duong', 
+    
+    # Tạo file mới
+     | Out-File -FilePath "html\.html" -Encoding UTF8
+    Write-Host "Created: html\.html"
+}
---
+++ create_pages_fixed.ps1
@@ -0,0 +1,30 @@
+﻿# Script tạo các trang tự động
+$pages = @{
+    'vo-sinh-nam' = 'Vô sinh nam'
+    'xuat-tinh-som' = 'Xuất tinh sớm'
+    'viem-bang-quang' = 'Viêm bàng quang'
+    'benh-tuyen-tien-liet' = 'Bệnh tuyến tiền liệt'
+    'tieu-nhieu' = 'Tiểu nhiều'
+    'viem-co-tu-cung' = 'Viêm cổ tử cung'
+    'viem-buong-trung' = 'Viêm buồng trứng'
+    'u-nang-buong-trung' = 'U nang buồng trứng'
+    'dau-bung-kinh' = 'Đau bụng kinh'
+    'benh-lau' = 'Bệnh lậu'
+    'benh-giang-mai' = 'Bệnh giang mai'
+}
+
+foreach ($page in $pages.GetEnumerator()) {
+    $filename = $page.Key
+    $title = $page.Value
+    
+    # Copy template từ liet-duong.html
+    $templateContent = Get-Content "html\liet-duong.html" -Raw
+    
+    # Thay thế title và các thông tin cụ thể
+    $newContent = $templateContent -replace 'Liệt dương', $title
+    $newContent = $newContent -replace 'liet-duong', $filename
+    
+    # Tạo file mới
+    $newContent | Out-File -FilePath "html\$filename.html" -Encoding UTF8
+    Write-Host "Created: html\$filename.html"
+}
